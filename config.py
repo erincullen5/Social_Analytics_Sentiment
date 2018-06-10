09bd0c8594666a743737fd5a054042cad85d406f
Erin Cullen <erin.cullen5@gmail.com>
2018-06-09 22:04:31 -0500

    updated jupyter notebook with markdown and graphs

---
+++ config.py
@@ -0,0 +1,5 @@
+# Erin's Twitter API Keys
+consumer_key = "Djyh8nQ2G5iIPgRPwMPPWxyyW"
+consumer_secret = "LPJKf02uqRV9mV1xuMgqXaInZxj1v0Ki2ywXuCaAcct5CogA4V"
+access_token = "297083995-Poopw2IAjjFxGoq5LBRL6cLY3RISplyLwVY608J9"
+access_token_secret = "g6BL4yH0VNEhArDqTnqtAWKgxpkOZqMwvGMbyusaR8tEe"
